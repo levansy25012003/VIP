a857b41523499a4c5f03959b00de5aa4b6dd0649
levansy25012003 <levansy25012003@gmail.com>
2024-05-15 13:55:01 +0700

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_CuoiKy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
